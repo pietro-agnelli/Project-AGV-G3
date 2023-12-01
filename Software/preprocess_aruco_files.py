2f209efa7225574a139ea11e9492bf7874570bf3
NotValcake <r.valtorta@studenti.unibs.it>
2023-12-01 17:39:28 +0100

    Added 2023-01-12 tests

--- Software/preprocess_aruco_files.py
+++ Software/preprocess_aruco_files.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import os, glob
 
-TESTS_DIR = './Tests/20231128/01_raw/'
-PREPROCESSED_DIR = './Tests/20231128/02_preprocessing'
+TESTS_DIR = './Tests/20231201/01_raw/'
+PREPROCESSED_DIR = './Tests/20231201/02_preprocessing'
 
-for test in glob.glob(os.path.join(TESTS_DIR, '*ARUCO*.csv')):
+for test in glob.glob(os.path.join(TESTS_DIR, '*__ARUCO*.csv')):
     df = pd.read_csv(test)
     for r in df.index:
         if df.loc[r,'id_marker'] > 7 or df.loc[r,'id_marker'] < 1:
