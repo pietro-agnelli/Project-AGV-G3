6337db924cb312b3b1b6739832c63eea810b464d
NotValcake <r.valtorta@studenti.unibs.it>
2023-12-13 16:27:50 +0100

    Published uncertainties

--- Software/odometry_linear_reg.m
+++ Software/odometry_linear_reg.m
@@ -6,28 +6,28 @@ close all
 PATH = "../Tests/20231201/02_preprocessing";
 
 % Translation along x axis 
-DATASET_ZX_PARALLEL_1 = readtable(PATH+"/POSE_DATA___ARUCO_2023_12_01_11_10_05.csv");
-DATASET_ZX_PARALLEL_2 = readtable(PATH+"/POSE_DATA___ARUCO_2023_12_01_11_14_05.csv");
+DATASET_ZX_PARALLEL_1 = readtable(PATH+"/POSE_DATA__2023_12_01_11_10_05.csv");
+DATASET_ZX_PARALLEL_2 = readtable(PATH+"/POSE_DATA__2023_12_01_11_14_05.csv");
 zx_dataset = DATASET_ZX_PARALLEL_2;%[DATASET_ZX_PARALLEL_1; DATASET_ZX_PARALLEL_2];
 
 % Translation along z axis
-DATASET_XZ_ORTHO_1 = readtable(PATH+"/POSE_DATA___ARUCO_2023_12_01_11_06_54.csv");
-DATASET_XZ_ORTHO_2 = readtable(PATH+"/POSE_DATA___ARUCO_2023_12_01_11_09_02.csv");
+DATASET_XZ_ORTHO_1 = readtable(PATH+"/POSE_DATA__2023_12_01_11_06_54.csv");
+DATASET_XZ_ORTHO_2 = readtable(PATH+"/POSE_DATA__2023_12_01_11_09_02.csv");
 xz_dataset = [DATASET_XZ_ORTHO_1; DATASET_XZ_ORTHO_2];
 
 % Translation 30 deg from z axis
-DATASET_THETA_30_1 = readtable(PATH+"/POSE_DATA___ARUCO_2023_12_01_11_03_14.csv");
-DATASET_THETA_30_2 = readtable(PATH+"/POSE_DATA___ARUCO_2023_12_01_11_05_08.csv");
+DATASET_THETA_30_1 = readtable(PATH+"/POSE_DATA__2023_12_01_11_03_14.csv");
+DATASET_THETA_30_2 = readtable(PATH+"/POSE_DATA__2023_12_01_11_05_08.csv");
 theta_30_dataset = [DATASET_THETA_30_1;DATASET_THETA_30_2];
 
 % Translation 45 deg from z axis
-DATASET_THETA_45_1 = readtable(PATH+"/POSE_DATA___ARUCO_2023_12_01_10_59_33.csv");
-DATASET_THETA_45_2 = readtable(PATH+"/POSE_DATA___ARUCO_2023_12_01_11_01_45.csv");
+DATASET_THETA_45_1 = readtable(PATH+"/POSE_DATA__2023_12_01_10_59_33.csv");
+DATASET_THETA_45_2 = readtable(PATH+"/POSE_DATA__2023_12_01_11_01_45.csv");
 theta_45_dataset = [DATASET_THETA_45_1;DATASET_THETA_45_2];
 
 % Translation 60 deg from z axis
-DATASET_THETA_60_1 = readtable(PATH+"/POSE_DATA___ARUCO_2023_12_01_10_55_40.csv");
-DATASET_THETA_60_2 = readtable(PATH+"/POSE_DATA___ARUCO_2023_12_01_10_57_50.csv");
+DATASET_THETA_60_1 = readtable(PATH+"/POSE_DATA__2023_12_01_10_55_40.csv");
+DATASET_THETA_60_2 = readtable(PATH+"/POSE_DATA__2023_12_01_10_57_50.csv");
 theta_60_dataset =[DATASET_THETA_60_1;DATASET_THETA_60_2];
 
 %% LINEAR REGRESSION
@@ -104,9 +104,10 @@ sigma_theta_60 = sqrt(gof_60.sse/(height(theta_60_dataset)-out_60.numparam))
 
 %% UNCERTAINTY MATRIX
 % Use the greatest uncertainty among theta
-Uaruco = diag([sigma_zx sigma_xz sigma_theta_0])
+Uodometry = diag([sigma_zx sigma_xz sigma_theta_60])
 
-writematrix(Uaruco,"../Tests/20231201/04_results/Uaruco.csv")
+% Expressing uncertainty in mm while odometry is performed in m
+writematrix(Uodometry*1000,"../Tests/20231201/04_results/Uodometry.csv")
 
 %% Uncertainty variation over theta
 figure("Name","Theta variance")
