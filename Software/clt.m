fb2303e71ef3e71a729faf38bac61c47e63d8424
NotValcake <r.valtorta@studenti.unibs.it>
2023-12-15 18:22:30 +0100

    Tested clt

--- Software/clt.m
+++ Software/clt.m
@@ -2,7 +2,7 @@ function [xf,sigmaf] = clt(x,sigma)
 %CLT applies the central limit theorem to the input vector
 %   
 w = (sigma.^(-2))./sum(sigma.^(-2));
-xf = x(1)*w(1)+x(2)*w(2);
+xf = x(1,:)*w(1)+x(2,:)*w(2);
 sigmaf = 1/sqrt(sum(sigma.^(-2)));
 end
 
