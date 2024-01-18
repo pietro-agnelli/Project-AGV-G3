57afeef8dd4c72fe830bc57817994d329db3fe73
pietro-agnelli <p.agnelli@studenti.unibs.it>
2024-01-18 14:39:09 +0100

    aggiunta propagazione classica con test (v2)

--- Software/uncertainty_prop_v2.m
+++ Software/uncertainty_prop_v2.m
@@ -1,4 +1,4 @@
-function [sigmax] = uncertainty_prop_v2(sigma2_0,nofmeasures, dt, theta, w, alpha, vel, acc)
+function [sigmax] = uncertainty_prop_v2(sigma_w, sigma_a,nofmeasures, dt, w, acc)
 %UNCERTAINTY_PROP Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -9,17 +9,31 @@ function [sigmax] = uncertainty_prop_v2(sigma2_0,nofmeasures, dt, theta, w, alph
 
 %dt: framerate [s]
 
+%ipotizzo distribuzione gaussiana, genero valori casuali distribuiti
+%normalmente (a, w); legge di moto genero posizione vel ecc e theta
+for i = 2: (nofmeasures)
+    theta = [theta theta(i-1)+ w(i-1)*dt];
+    vel = [vel vel(i-1)+acc(i-1)*dt];
+
+end
+
 %vettori delle varianze delle grandezze lineari e angolari, ponendo
 %la sigma2 attuale come quella del sensore (sigma0)
-sigma2x = [ 0 0 sigma2_0(1,1)];
-sigma2v = [0 0 sigma2_0(1,2) ];
-sigma2a =sigma2_0(1,3);
-sigma2theta =[ 0 0 sigma2_0(2,1)];
-sigma2_w =sigma2_0(2,2);
-sigma2alpha =[ 0 0 sigma2_0(2,3)];
-for n = 4:nofmeasures
-    sigma2v = [sigma2v sigma2v(n-1) + sqrt(sigma_a)];
-    sigma2theta = [sigma2theta sigma2theta(n-1) + sqrt(sigma_w)];
-    sigmax = [sigmax sqrt(sigma2x(n-1) + dt^2*cos(theta(n-2)+w(n-2)*dt)^2*(sigma2v(n-3)+sqrt(sigma2a)*dt^2)+9/4*dt^4*cos(theta(n-2)+w(n-2)*dt)^2*sigma2a + (vel(n-2)*dt + 3/2*acc(n-2)*dt^2)^2*sin(theta(n-2)+w(n-2)*dt)^2*(sigma2theta(n-3)+sigma_w*dt^2)+dt^2*(vel(n-2)*dt + 3/2*acc(n-2)*dt^2)^2*sin(theta(n-2)+w(n-2)*dt)^2)];
+sigmax  = [];
+sigma2x = [0 0 0];
+sigma2v = [0 0 0];
+sigma2a =(sigma_a)^2;
+sigma2theta =[ 0 0 0];
+sigma2_w  = (sigma_w)^2;
+for n = 4:(nofmeasures)
+    sigma2v = [sigma2v sigma2v(n-1) + sqrt(sigma2a)];
+    sigma2theta = [sigma2theta sigma2theta(n-1) + sqrt(sigma2_w)];
+end
+for n = 4 :1: (nofmeasures)
+    sigma2x(n) =sigma2x(n-1) + ...
+        (dt^2)*cos(theta(n-2)+w(n-2)*dt)^2*(sigma2v(n-3)+sqrt(sigma2a)*dt^2)+...
+        (9/4)*(dt^4)*cos(theta(n-2)+w(n-2)*dt)^2*sigma2a + ...
+        (vel(n-2)*dt +3/2*acc(n-2)*dt^2)^2*sin(theta(n-2)+w(n-2)*dt)^2*(sigma2theta(n-3)+sigma_w*dt^2)+ ...
+        dt^2*(vel(n-2)*dt + 3/2*acc(n-2)*dt^2)^2*sin(theta(n-2)+w(n-2)*dt)^2*sigma_w^2;
 end
 end
